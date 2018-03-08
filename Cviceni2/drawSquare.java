f2156a5bbc8b3af08e0722c5de527088af9d2783
petr7555 <peta.janik@email.cz>
2018-03-08 21:45:25 +0100

    updated square program

--- Cviceni2/drawSquare.java
+++ Cviceni2/drawSquare.java
@@ -2,15 +2,20 @@ public class drawSquare {
 
     public static void main(String args[]) {
         int size = 5;
+        Boolean full = false;
 
         for (int i = 1; i <= size; i++) {
             for (int j = 1; j <= size; j++) {
-                if (i == 1 || i == size) {
-                    System.out.print("#");
-                } else if (j == 1 || j == size) {
-                    System.out.print("#");
+                if (full == false) {
+                    if (i == 1 || i == size) {
+                        System.out.print("#");
+                    } else if (j == 1 || j == size) {
+                        System.out.print("#");
+                    } else {
+                        System.out.print(" ");
+                    }
                 } else {
-                    System.out.print(" ");
+                    System.out.print("#");
                 }
 
             }
